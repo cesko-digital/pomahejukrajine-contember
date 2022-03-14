5d4d0929f7ea1b7c28e12b9153294af2a8fd292b
jonasNobile <jonas.nobile@icloud.com>
2022-03-14 17:02:10 +0100

    Add status unique types

--- admin/pages/settings.tsx
+++ admin/pages/settings.tsx
@@ -31,6 +31,16 @@ export const tags = (
 export const status = (
 	<MultiEditPage entities="OfferStatus" rendererProps={{ sortableBy: "order", title: "Stavy nabídek" }}>
 		<TextField field="name" label="Název" />
+		<SelectField
+			field="type"
+			label="Typ"
+			allowNull
+			options={[
+				{ label: "Kapacity vyčerpána", value: "capacity_exhausted" },
+				{ label: "Špatná zkušenost s nabídkou", value: "bad_experience" },
+				{ label: "Již není aktuální", value: "outdated" },
+			]}
+		/>
 	</MultiEditPage>
 )
 
