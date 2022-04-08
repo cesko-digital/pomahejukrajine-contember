bbed7f1c546f5062941f99adf7fdd5356f4202d6
Honza Sládek <me@honzasladek.com>
2022-04-08 11:18:04 +0200

    Add role conditional to menu to hide stuff you cant use

--- admin/components/Navigation.tsx
+++ admin/components/Navigation.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Menu, EntityListSubTree, FieldView, DataBindingProvider, FeedbackRenderer, LogoutLink } from '@contember/admin'
+import { RoleConditional } from './RoleConditional'
 
 export const Navigation = () => (
 	<Menu>
@@ -8,7 +9,9 @@ export const Navigation = () => (
 		{/*</Menu.Item>*/}
 		<Menu.Item title="Dobrovolníci">
 			<Menu.Item title="Seznam" to="volunteers" />
-			<Menu.Item title="Registrovat" to="newVolunteer" />
+			<RoleConditional role={['admin', 'organizationAdmin']}>
+				<Menu.Item title="Registrovat" to="newVolunteer" />
+			</RoleConditional>
 		</Menu.Item>
 		<Menu.Item title="Nabídky">
 			<DataBindingProvider stateComponent={FeedbackRenderer} refreshOnEnvironmentChange={false}>
@@ -25,15 +28,17 @@ export const Navigation = () => (
 		<Menu.Item title="Žádosti">
 			<Menu.Item title="Seznam" to="demandList" />
 		</Menu.Item>
-		<Menu.Item title="Nastavení">
-			<Menu.Item title="Typy nabídek" to="offerTypes" />
-			<Menu.Item title="Tagy dobrovolníků" to="tags" />
-			<Menu.Item title="Kraje" to="districts" />
-			<Menu.Item title="Jazyky" to="languages" />
-			<Menu.Item title="Organizace" to="organizations" />
-			<Menu.Item title="Pracovníci" to="organizationManagerList" />
-			<Menu.Item title="Stavy" to="status" />
-		</Menu.Item>
+		<RoleConditional role={['admin', 'organizationAdmin']}>
+			<Menu.Item title="Nastavení">
+				<Menu.Item title="Typy nabídek" to="offerTypes" />
+				<Menu.Item title="Tagy dobrovolníků" to="tags" />
+				<Menu.Item title="Kraje" to="districts" />
+				<Menu.Item title="Jazyky" to="languages" />
+				<Menu.Item title="Organizace" to="organizations" />
+				<Menu.Item title="Pracovníci" to="organizationManagerList" />
+				<Menu.Item title="Stavy" to="status" />
+			</Menu.Item>
+		</RoleConditional>
 		<Menu.Item title="Uživatel">
 			<Menu.Item title="Změnit heslo" href="/_panel/security" />
 			<LogoutLink><Menu.Item title="Odhlásit se" /></LogoutLink>
