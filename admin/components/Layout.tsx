768b8a6550ac14221028e7e9781b6a3a27855a8b
jonasNobile <jonas.nobile@icloud.com>
2022-04-12 18:16:18 +0200

    Add toggle menu button

--- admin/components/Layout.tsx
+++ admin/components/Layout.tsx
@@ -2,11 +2,22 @@ import * as React from 'react'
 import { ReactNode } from 'react'
 import { Layout as ContemberLayout } from '@contember/admin'
 import { Navigation } from './Navigation'
+import './layout.sass'
 
-export const Layout = (props: { children?: ReactNode }) => (
-	<ContemberLayout
-		sidebarHeader="Pomáhej Ukrajině"
-		navigation={<Navigation />}
-		children={props.children}
-	/>
-)
+export const Layout = (props: { children?: ReactNode }) => {
+	const [isNavigationOpen, setIsNavigationOpen] = React.useState(true)
+	return (
+		<ContemberLayout
+			sidebarHeader={isNavigationOpen ? "Pomáhej Ukrajině" : undefined}
+			navigation={isNavigationOpen ? <Navigation /> : undefined}
+			children={
+				<>
+					<button onClick={() => setIsNavigationOpen(!isNavigationOpen)} className="side-menu-toggle">
+						{isNavigationOpen ? <span><span>{'<'}</span> <span className="hidden">hide menu</span></span> : <span><span>{'>'}</span> <span className="hidden">show menu</span></span> }
+					</button>
+					{props.children}
+				</>
+			}
+		/>
+	)
+}
