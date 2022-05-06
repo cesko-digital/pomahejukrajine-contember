650b788d8dc06fd3f336afbe478b00098d17f641
Honza Sládek <me@honzasladek.com>
2022-05-06 15:22:45 +0200

    Add more fields to Organizations datagrid

--- admin/pages/organizations.tsx
+++ admin/pages/organizations.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import {DataGridPage, EnumCell, GenericCell, LinkButton, TextCell} from "@contember/admin"
+import {DataGridPage, EnumCell, GenericCell, HasOneSelectCell, LinkButton, TextCell} from "@contember/admin"
 
 export default (
 	<DataGridPage entities="Organization" itemsPerPage={50} rendererProps={{ title: "Organizace", actions: <LinkButton to="organizationCreate">Přidat organizaci</LinkButton> }}>
@@ -22,6 +22,13 @@ export default (
 									volunteerInitiative: 'Dobrovolnická iniciativa',
 								}}
 							header="Typ organizace" />
+		<TextCell field="identificationNumber" header="IČ" />
+		<TextCell field="address" header="Adresa" hidden />
+		<HasOneSelectCell header="Kraje" field="region" options="Region.name" />
+		<HasOneSelectCell header="Okres" field="district" options="District.name" />
+		<TextCell header="Web" field="website" hidden />
+		<TextCell header="Zkratka" field="nickname" hidden />
+		<TextCell header="Poznámka" field="note" hidden />
 		<GenericCell shrunk>
 			<LinkButton to="organizationEdit(id: $entity.id)">Upravit</LinkButton>
 		</GenericCell>
