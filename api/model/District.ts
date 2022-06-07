066f5382b8002e189c69d1288df53aa7a5febef7
Honza Sládek <me@honzasladek.com>
2022-06-07 14:24:38 +0200

    Make UK districts unique

--- api/model/District.ts
+++ api/model/District.ts
@@ -7,7 +7,7 @@ import { VolunteerDistrict } from './Volunteer'
 export class District { // Okres
 	region = def.manyHasOne(Region, 'districts').notNull()
 	name = def.stringColumn().notNull().unique()
-	nameUK = def.stringColumn().notNull().default('')
+	nameUK = def.stringColumn().notNull().unique().default('')
 	volunteers = def.oneHasMany(VolunteerDistrict, 'district')
 	offerParameterValues = def.oneHasMany(OfferParameterValueDetails, 'district')
 	offers = def.oneHasMany(OfferParameterValue, 'district')
